e16c4a7890f87626732df25fbcfbfa8352ad06eb
Udhaya kumar <kingudhaya336@gmail.com>
2026-06-19 10:30:58 +0530

    Time: 1 ms (99.26%), Space: 42.6 MB (73.82%) - LeetHub

---
+++ 3945-digit-frequency-score/3945-digit-frequency-score.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int digitFrequencyScore(int n) {
+        int m=n;
+        int sum=0;
+        while(n>0)
+        {
+            int d=n%10;
+            sum+=d;
+             n/=10;
+        }
+        return sum;
+    }
+}
