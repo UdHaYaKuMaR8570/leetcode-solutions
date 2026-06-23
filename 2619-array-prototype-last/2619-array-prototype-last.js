8449f07eea29298016c2e550167d693e07feda7c
Udhaya kumar <kingudhaya336@gmail.com>
2026-06-23 11:54:38 +0530

    Time: 49 ms (16.58%), Space: 53.9 MB (24.41%) - LeetHub

---
+++ 2619-array-prototype-last/2619-array-prototype-last.js
@@ -0,0 +1,16 @@
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+    // return array.last();
+    if(this.length===0)
+    {
+        return -1;
+    }
+    return this [this.length-1];
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
