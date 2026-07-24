ccf9585e4cb5b5427f05ce169dad04304329d3b6
Udhaya kumar <kingudhaya336@gmail.com>
2026-07-24 14:18:42 +0530

    Time: 1 ms (98.6%), Space: 44.1 MB (12.59%) - LeetHub

---
+++ 1576-replace-all-s-to-avoid-consecutive-repeating-characters/1576-replace-all-s-to-avoid-consecutive-repeating-characters.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String modifyString(String s) {
+        char arr[] = s.toCharArray();
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]=='?')
+            {
+                for(char c='a';c<='z';c++)
+                {
+                    if((i==0||arr[i-1]!=c) && (i==arr.length-1||arr[i+1]!=c))
+                    {
+                        arr[i]=c;
+                        break;
+                    }
+                }
+            }
+        }
+        return new String(arr);
+        //     if(c=='?')
+        //     {
+        //         for(char c='a';char<'z';c++){
+        //             if((i==0 ||arr[i] ))
+        //         }
+        //     }
+        // }
+    }
+}
